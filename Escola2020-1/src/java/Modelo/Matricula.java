dbd443d42a613f7fcc32a8b3c3e195bbaaf590bd
kiko hirano <kikopalmas@gmail.com>
2020-05-12 15:53:10 -0300

    inicio do projeto Escola 2020-1

---
+++ Escola2020-1/src/java/Modelo/Matricula.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author kikohirano
+ */
+public class Matricula {
+    private Integer id;
+    private Aluno aluno;
+    private Turma turma;
+    private Float frequencia;
+    private Float nota;
+    private String status;
+    
+}
---
+++ Escola2020-1/src/java/Modelo/Professor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author kikohirano
+ */
+public class Professor {
+    
+    private Integer id;
+    private String nomeProfessor;
+    private String titulo;
+    
+}
