4f5f79a55b1680d3fc15435f4679702cfedc0c83
Thányla Sales <thanylas@gmail.com>
2020-05-13 08:54:31 -0300

    Inserção Getters e Setters classe Aluno

--- Escola2020-1/src/java/Modelo/Aluno.java
+++ Escola2020-1/src/java/Modelo/Aluno.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modelo;
 
 import java.util.Date;
 
 /**
  *
- * @author kikohirano
+ * @author Thányla Sales
  */
 public class Aluno {
     private Integer id;
@@ -18,5 +13,55 @@ public class Aluno {
     private String cpf;
     private String endereco;
     private Date dataNascimento;
+    
+    //GETTERS E SETTRS
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
     
 }
